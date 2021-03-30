38a8d0c29003d8d034d2bc6cced91e1a4950f326
Anisoi Iulia Roxana <iulia@192-168-0-105.rdsnet.ro>
2021-03-30 21:55:16 +0300

    first commit

---
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Anisoi Iulia Roxana on 05.03.2021.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {}
+
+}
+
